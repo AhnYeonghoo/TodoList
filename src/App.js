33e48b2180b5c5738af5612faa0bba22c48e7f6c
AhnYeonghoo <dksdudgn00@naver.com>
2023-01-05 16:54:38 +0900

    DarkMode Impl

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import TodoList from "./components/TodoList/TodoList";
+import { DarkModeProvider } from "./context/DarkModeContext";
 
 const filters = ["all", "active", "completed"];
 
 function App() {
 	const [filter, setFilter] = useState(filters[0]);
 	return (
-		<>
+		<DarkModeProvider>
 			<Header filters={filters} filter={filter} onFilterChange={setFilter} />
 			<TodoList filter={filter} />
-		</>
+		</DarkModeProvider>
 	);
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+import { useDarkMode } from "../../context/DarkModeContext";
 import styles from "./Header.module.css";
+import { HiMoon, HiSun } from "react-icons/hi";
+
 export default function Header({ filters, filter, onFilterChange }) {
+	const { darkMode, toggleDarkMode } = useDarkMode();
+
 	return (
 		<header className={styles.header}>
+			<button onClick={toggleDarkMode} className={styles.toggle}>
+				{!darkMode && <HiMoon />}
+				{darkMode && <HiSun />}
+			</button>
 			<ul className={styles.filters}>
 				{filters.map((value, index) => (
 					<li key={index}>
